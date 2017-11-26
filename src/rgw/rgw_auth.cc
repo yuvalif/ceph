2ccb8c1af285488e5097e8839f31a832392fed18
Marcus Watts <mwatts@redhat.com>
2017-11-26 18:43:23 -0500

    radosgw: fix swift anonymous access.
    With swift: when accessing a bucket anonymously (no credentials),
    must set the bucket tenant appropriately in order to access the container.
    
    Fixes: http://tracker.ceph.com/issues/22259
    resolves: rhbz#2123177
    
    Signed-off-by: Marcus Watts <mwatts@redhat.com>
    (cherry picked from commit 3f58bb08a74344d5e945b160da12c2872d58c78a)
    
    Conflicts:
    	src/rgw/rgw_auth.cc
    	src/rgw/rgw_rest_swift.cc

--- src/rgw/rgw_auth.cc
+++ src/rgw/rgw_auth.cc
@@ -916,9 +916,20 @@ void rgw::auth::RoleApplier::modify_request_state(const DoutPrefixProvider *dpp,
   }
 }
 
+struct AnonymousEngineFixit : public rgw::auth::Completer {
+//  AnonymousEngineFixit() { }
+  bool complete() override { return false; }
+  void modify_request_state(const DoutPrefixProvider* dpp, req_state* s_rw) override {
+    if (! s_rw->account_name.empty()) {
+      s_rw->bucket_tenant = s_rw->account_name;
+    }
+  }
+};
+
 rgw::auth::Engine::result_t
 rgw::auth::AnonymousEngine::authenticate(const DoutPrefixProvider* dpp, const req_state* const s, optional_yield y) const
 {
+  typedef std::shared_ptr<Completer> cmplptr_t;
   if (! is_applicable(s)) {
     return result_t::deny(-EPERM);
   } else {
@@ -929,6 +940,7 @@ rgw::auth::AnonymousEngine::authenticate(const DoutPrefixProvider* dpp, const re
       apl_factory->create_apl_local(cct, s, user_info,
                                     rgw::auth::LocalApplier::NO_SUBUSER,
                                     std::nullopt, rgw::auth::LocalApplier::NO_ACCESS_KEY);
-    return result_t::grant(std::move(apl));
+    auto cpl = AnonymousEngineFixit();
+    return result_t::grant(std::move(apl), cmplptr_t(new decltype(cpl)(std::move(cpl))));
   }
 }
