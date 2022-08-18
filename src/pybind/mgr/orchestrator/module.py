510398d0759647bf9a016e11d3c5c60985e08dd5
Redouane Kachach <rkachach@redhat.com>
2022-08-18 15:33:21 +0200

    mgr/rgw: Adding rgw multisite support Fixes: https://tracker.ceph.com/issues/57160
    Signed-off-by: Redouane Kachach <rkachach@redhat.com>
    (cherry picked from commit d15a5dcfe2d05240059b9d4a72c581d867d5e7a4)
    
    Conflicts:
    	doc/mgr/rgw.rst
    (cherry picked from commit 52c5cff6e18cff841219ad02e40c5d8698ea4ad4)
    
    Resolves: rhbz#2016288
    Resolves: rhbz#2109224

--- src/pybind/mgr/orchestrator/module.py
+++ src/pybind/mgr/orchestrator/module.py
@@ -1110,6 +1110,7 @@ Usage:
                    placement: Optional[str] = None,
                    _end_positional_: int = 0,
                    realm: Optional[str] = None,
+                   zonegroup: Optional[str] = None,
                    zone: Optional[str] = None,
                    port: Optional[int] = None,
                    ssl: bool = False,
@@ -1132,6 +1133,7 @@ Usage:
         spec = RGWSpec(
             service_id=svc_id,
             rgw_realm=realm,
+            rgw_zonegroup=zonegroup,
             rgw_zone=zone,
             rgw_frontend_port=port,
             ssl=ssl,
