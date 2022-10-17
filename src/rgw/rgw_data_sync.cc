2a63baf84b3bc533805c469ff85939f3936f5afa
Shilpa Jagannath <smanjara@redhat.com>
2022-10-17 15:07:49 -0400

    rgw/multisite:  marker_tracker->finish() returns error
    Fixes rhbz#2129718
    Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>
    (cherry picked from commit e4ccc11631b1d0c2a7801e8fa453ff383f27bfbf)
    Resolves: rhbz#2075212
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/rgw_data_sync.cc
+++ src/rgw/rgw_data_sync.cc
@@ -1471,6 +1471,9 @@ public:
       if (marker_tracker && !complete->marker.empty()) {
         /* update marker */
         yield call(marker_tracker->finish(complete->marker));
+        if (retcode < 0) {
+          return set_cr_error(retcode);
+        }
       }
       if (sync_status == 0) {
         sync_status = retcode;
@@ -1690,6 +1693,9 @@ public:
       }
 
       yield call(marker_tracker->finish(key));
+      if (retcode < 0) {
+          return set_cr_error(retcode);
+        }
 
       return set_cr_done();
     }
