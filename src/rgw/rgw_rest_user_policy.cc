a2db5edbe9cb6783a8e0f79ba32cd52f3976b183
Adam C. Emerson <aemerson@redhat.com>
2022-12-13 16:15:24 -0500

    rgw: Put useful policy error messages in error document
    This affects the various create/put operations that take a policy document.
    
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>
    (cherry picked from commit 0b0fd44f42575c89ccce375b06c0db1d50c48ce6)
    Resolves: rhbz#2153775
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/rgw_rest_user_policy.cc
+++ src/rgw/rgw_rest_user_policy.cc
@@ -159,7 +159,8 @@ void RGWPutUserPolicy::execute(optional_yield y)
       op_ret = -ERR_INTERNAL_ERROR;
     }
   } catch (rgw::IAM::PolicyParseException& e) {
-    ldpp_dout(this, 20) << "failed to parse policy: " << e.what() << dendl;
+    ldpp_dout(this, 5) << "failed to parse policy: " << e.what() << dendl;
+    s->err.message = e.what();
     op_ret = -ERR_MALFORMED_DOC;
   }
 
