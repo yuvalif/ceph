7ce0068a6c583aa0e1e354a5011b001a04d6aa4f
Patrick Donnelly <pdonnell@redhat.com>
2023-04-05 15:25:38 -0400

    qa: load file system info if not created
    Mounts may create a Filesystem object which does not create the file
    system. This causes self.id to be Null when we try to wait for daemons
    to be up. Load it just in time!
    
    Fixes: https://tracker.ceph.com/issues/59332
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>
    (cherry picked from commit 69565dbbf124e29f30dae989eae41eada161a4b4)
    (cherry picked from commit 27bbbc2112e34cb21be0c5a2cb00d4a3bf4e901d)
    
    Resolves: rhbz#215307

--- qa/tasks/cephfs/filesystem.py
+++ qa/tasks/cephfs/filesystem.py
@@ -1093,6 +1093,9 @@ class Filesystem(MDSCluster):
         if timeout is None:
             timeout = DAEMON_WAIT_TIMEOUT
 
+        if self.id is None:
+            status = self.getinfo(refresh=True)
+
         if status is None:
             status = self.status()
 
