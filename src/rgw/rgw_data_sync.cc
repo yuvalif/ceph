thotz/ceph-ci

rgw: rename to rgw_read_bucket_inc_sync_status
Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/rgw/rgw_data_sync.cc
@@ -4999,12 +4999,12 @@ class RGWCollectBucketSyncStatusCR : public RGWShardCollectCR {
   }
 };
 
-int rgw_bucket_sync_status(const DoutPrefixProvider *dpp,
-                           rgw::sal::RGWRadosStore *store,
-                           const rgw_sync_bucket_pipe& pipe,
-                           const RGWBucketInfo& dest_bucket_info,
-                           const RGWBucketInfo *psource_bucket_info,
-                           std::vector<rgw_bucket_shard_sync_info> *status)
+int rgw_read_bucket_inc_sync_status(const DoutPrefixProvider *dpp,
+                                    rgw::sal::RGWRadosStore *store,
+                                    const rgw_sync_bucket_pipe& pipe,
+                                    const RGWBucketInfo& dest_bucket_info,
+                                    const RGWBucketInfo *psource_bucket_info,
+                                    std::vector<rgw_bucket_shard_sync_info> *status)
 {
   if (!pipe.source.zone ||
       !pipe.source.bucket ||

yuvalif/ceph

(cherry picked from commit 1708360b39d6c8ae323236fef97a637d497954f8)
Resolves: rhbz#2075212
Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

src/rgw/rgw_data_sync.cc
@@ -4973,12 +4973,12 @@ class RGWCollectBucketSyncStatusCR : public RGWShardCollectCR {
   }
 };
 
-int rgw_bucket_sync_status(const DoutPrefixProvider *dpp,
-                           rgw::sal::RadosStore* store,
-                           const rgw_sync_bucket_pipe& pipe,
-                           const RGWBucketInfo& dest_bucket_info,
-                           const RGWBucketInfo *psource_bucket_info,
-                           std::vector<rgw_bucket_shard_sync_info> *status)
+int rgw_read_bucket_inc_sync_status(const DoutPrefixProvider *dpp,
+                                    rgw::sal::RadosStore *store,
+                                    const rgw_sync_bucket_pipe& pipe,
+                                    const RGWBucketInfo& dest_bucket_info,
+                                    const RGWBucketInfo *psource_bucket_info,
+                                    std::vector<rgw_bucket_shard_sync_info> *status)
 {
   if (!pipe.source.zone ||
       !pipe.source.bucket ||