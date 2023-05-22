f883d493627d56e90b8c03363f18b89e7a20244a
Adam Kupczyk <akupczyk@ibm.com>
2023-05-22 19:24:52 +0200

    Revert "os/bluestore: Add #if WITH_ESB to unittests"
    This reverts commit ed453e06089673f086a8d095d533535d559f6617.
    
    Resolves: rhbz#2119217

--- src/test/objectstore/test_bluestore_types.cc
+++ src/test/objectstore/test_bluestore_types.cc
@@ -1332,9 +1332,6 @@ public:
   }
 
   void SetUp() override {
-#ifndef WITH_ESB
-    GTEST_SKIP() << "ESB(elastic shared blob) is off";
-#endif
   }
   void TearDown() override {
   }
@@ -1665,9 +1662,6 @@ TEST_F(ExtentMapFixture, petri)
 
 TEST(ExtentMap, dup_extent_map)
 {
-#ifndef WITH_ESB
-  GTEST_SKIP() << "ESB(elastic shared blob) is off";
-#endif
   BlueStore store(g_ceph_context, "", 4096);
   BlueStore::OnodeCacheShard *oc = BlueStore::OnodeCacheShard::create(
     g_ceph_context, "lru", NULL);
