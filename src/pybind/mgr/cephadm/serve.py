abb269d8f7df7380f68f52251897130c1d950aaa
John Mulligan <jmulligan@redhat.com>
2023-05-24 14:11:52 -0400

    mgr/cephadm: allow passing a list as a compound command to _run_cephadm
    As we're adding "namespaces" to the cephadm command it's easier to work
    with a compound list of terms as the command. This also avoids an issue
    where the function call injects args between the command and the "args"
    passed to _run_cephadm.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>
    (cherry picked from commit 6f12ddbb40d9ef5d57407390302af4d566a619d9)
    
    Resolves: rhbz#2231452

--- src/pybind/mgr/cephadm/serve.py
+++ src/pybind/mgr/cephadm/serve.py
@@ -1462,7 +1462,7 @@ class CephadmServe:
     async def _run_cephadm(self,
                            host: str,
                            entity: Union[CephadmNoImage, str],
-                           command: str,
+                           command: Union[str, List[str]],
                            args: List[str],
                            addr: Optional[str] = "",
                            stdin: Optional[str] = "",
@@ -1514,7 +1514,10 @@ class CephadmServe:
         final_args += ['--timeout', str(timeout)]
 
         # subcommand
-        final_args.append(command)
+        if isinstance(command, list):
+            final_args.extend([str(v) for v in command])
+        else:
+            final_args.append(command)
 
         # subcommand args
         if not no_fsid:
