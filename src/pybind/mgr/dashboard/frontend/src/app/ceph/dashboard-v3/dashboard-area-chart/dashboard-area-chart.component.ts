6a4a8c17340115be3890bae80f1a9463d76f26dc
Pedro Gonzalez Gomez <pegonzal@redhat.com>
2023-07-07 10:07:56 +0200

    mgr/dashboard: Add more decimals in latency graph
    Resolves: rhbz#2238472
    
    Fixes: https://tracker.ceph.com/issues/61930
    
    Signed-off-by: Pedro Gonzalez Gomez <pegonzal@redhat.com>
    (cherry picked from commit 8c295e38ffd50c58c0d7cda0dbfe51864ab0e906)
    (cherry picked from commit b92dab9e998c1c877dedb58013158f7e32ee3c6b)

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard-v3/dashboard-area-chart/dashboard-area-chart.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard-v3/dashboard-area-chart/dashboard-area-chart.component.ts
@@ -30,6 +30,8 @@ export class DashboardAreaChartComponent implements OnChanges, AfterViewInit {
   label: string;
   @Input()
   label2?: string;
+  @Input()
+  decimals?: number = 1;
 
   currentDataUnits: string;
   currentData: number;
@@ -214,7 +216,8 @@ export class DashboardAreaChartComponent implements OnChanges, AfterViewInit {
         dataWithUnits = this.numberFormatter.formatSecondsFromTo(
           data,
           this.dataUnits,
-          this.chartDataUnits
+          this.chartDataUnits,
+          this.decimals
         );
       } else {
         dataWithUnits = this.numberFormatter.formatUnitlessFromTo(
@@ -234,7 +237,7 @@ export class DashboardAreaChartComponent implements OnChanges, AfterViewInit {
     } else if (this.dataUnits === 'B/s') {
       dataWithUnits = this.dimlessBinaryPerSecond.transform(data);
     } else if (this.dataUnits === 'ms') {
-      dataWithUnits = this.formatter.format_number(data, 1000, ['ms', 's']);
+      dataWithUnits = this.formatter.format_number(data, 1000, ['ms', 's'], this.decimals);
     } else {
       dataWithUnits = this.dimlessPipe.transform(data);
     }
