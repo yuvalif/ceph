23a1613c21c934ab2a3415745f4cd592b7af1878
Kamoltat <ksirivad@redhat.com>
2023-07-13 18:23:13 +0000

    pybind/mgr/pg_autoscaler: noautoscale flag retains individual pool configs
    Problem:
    
    The pg_autoscaler `noautoscale flag` doesn't retain individual pool states of
    `autoscale mode`. For example turn the flag `ON` and then `OFF` again all
    the pools will have `autoscale mode on` which is inconvenience for the user
    because sometimes the user just want to temporary disable the autoscaler on
    all pools and will enable it back after a period of time while retaining
    individual pool states of `autoscale mode`
    
    Solution:
    
    We store noautoscale flag in the OSDMAP such that it is
    persistent. We then get rid of noautoscale MODULE OPTION
    in the pg_autoscaler module since we do not need it anymore.
    Everytime we set, unset or get the flag we rely on looking up
    the OSDMAP, we did this because we want to avoid inconsistancy
    between the `noautoscale flag`. This is because `noautoscale flag`
    can easily be set by doing `ceph osd set noautoscale`.
    
    Fixes: https://tracker.ceph.com/issues/61922
    
    Signed-off-by: Kamoltat <ksirivad@redhat.com>
    (cherry picked from commit 0bfdf6362a6b9705212705ee96fa3d6933e51437)
    
    Conflicts:
    	src/pybind/mgr/pg_autoscaler/module.py - trivial fix
    (cherry picked from commit 2371e197374e37a7fe91add5bf4ef96e026b6996)
    
    Resolves: rhbz#2241201

--- src/osd/OSDMap.cc
+++ src/osd/OSDMap.cc
@@ -3947,6 +3947,8 @@ string OSDMap::get_flag_string(unsigned f)
     s += ",purged_snapdirs";
   if (f & CEPH_OSDMAP_PGLOG_HARDLIMIT)
     s += ",pglog_hardlimit";
+  if (f & CEPH_OSDMAP_NOAUTOSCALE)
+    s += ",noautoscale";
   if (s.length())
     s.erase(0, 1);
   return s;
