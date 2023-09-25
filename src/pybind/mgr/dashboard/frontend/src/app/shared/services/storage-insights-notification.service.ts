3e45eac2d76349e44353a01f7339fe2255ad363b
Nizamudeen A <nia@redhat.com>
2023-09-25 17:12:10 +0530

    mgr/dashboard: fix call home endpoint getting pinged in rhcs
    call home endpoint getting pinged in the rhcs environment while
    reloading the dashboard page. I am putting extra checks to avoid that.
    Also takes care of some minor doc nits.
    
    Resolves: rhbz#2240189
    Signed-off-by: Nizamudeen A <nia@redhat.com>
    (cherry picked from commit 6b8fa8a41642e43fa18f9cd833e87d5385d0d8ec)

--- src/pybind/mgr/dashboard/frontend/src/app/shared/services/storage-insights-notification.service.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/services/storage-insights-notification.service.ts
@@ -2,6 +2,7 @@ import { EventEmitter, Injectable, OnDestroy, Output } from '@angular/core';
 import { Observable, BehaviorSubject, Subscription, of } from 'rxjs';
 import { mergeMap, tap } from 'rxjs/operators';
 import { StorageInsightsService } from '../api/storage-insights.service';
+import { environment } from '~/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,7 @@ import { StorageInsightsService } from '../api/storage-insights.service';
 export class StorageInsightsNotificationService implements OnDestroy {
   public remindLaterOn$: Observable<boolean>;
   public remindLaterOnSource = new BehaviorSubject<boolean>(false);
+  private environment = environment;
 
   @Output()
   update: EventEmitter<boolean> = new EventEmitter<boolean>();
@@ -16,26 +18,27 @@ export class StorageInsightsNotificationService implements OnDestroy {
   private subscription: Subscription;
   constructor(private storageInsightsService: StorageInsightsService) {
     this.remindLaterOn$ = this.remindLaterOnSource.asObservable();
-
-    this.subscription = of(true)
-      .pipe(
-        mergeMap(() =>
-          this.storageInsightsService.getStorageInsightsStatus().pipe(
-            mergeMap((status) => {
-              if (!status) return this.storageInsightsService.getStorageInsightsConfig();
-              else return of('never');
-            }),
-            tap((remindLaterOn: string) => {
-              const dateNow = new Date().toDateString();
-              const visible =
-                remindLaterOn === dateNow || remindLaterOn === '' || remindLaterOn === null;
-              this.remindLaterOnSource.next(visible);
-              this.update.emit(visible);
-            })
+    if (this.environment.build === 'ibm') {
+      this.subscription = of(true)
+        .pipe(
+          mergeMap(() =>
+            this.storageInsightsService.getStorageInsightsStatus().pipe(
+              mergeMap((status) => {
+                if (!status) return this.storageInsightsService.getStorageInsightsConfig();
+                else return of('never');
+              }),
+              tap((remindLaterOn: string) => {
+                const dateNow = new Date().toDateString();
+                const visible =
+                  remindLaterOn === dateNow || remindLaterOn === '' || remindLaterOn === null;
+                this.remindLaterOnSource.next(visible);
+                this.update.emit(visible);
+              })
+            )
           )
         )
-      )
-      .subscribe();
+        .subscribe();
+    }
   }
 
   hide() {
