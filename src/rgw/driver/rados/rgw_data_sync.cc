5ab129d2a9ae761ca84b0622c612a8bbbe97b48c
Shilpa Jagannath <smanjara@redhat.com>
2023-09-26 11:44:23 -0400

    rgw/multisite: call drain before flushing markers in incremental sync
    Resolves: rhbz#2240003
    Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>
    (cherry picked from commit 595aeb542c1ace5829e94730f99c49b94257d9bd)

--- src/rgw/driver/rados/rgw_data_sync.cc
+++ src/rgw/driver/rados/rgw_data_sync.cc
@@ -2115,11 +2115,12 @@ public:
 	}
       } while (true);
 
+      drain_all();
+      yield marker_tracker->flush();
+
       if (lost_bid) {
         return set_cr_error(-EBUSY);
       } else if (lost_lock) {
-        drain_all();
-        yield marker_tracker->flush();
         return set_cr_error(-ECANCELED);
       }
 
