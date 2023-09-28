186d946288ce5dc6d8f6c33174852688b767b521
Adam King <adking@redhat.com>
2023-09-27 21:54:01 -0400

    cephadm: change mount point for ceph-nvmeof.conf
    In the downstream nvmeof container, the nvmeof code
    is rooted at /remote-source/ceph-nvmeof/app/ rather
    than /src/ as it is in the upstream. This change adjusts
    the mount point in the container for the ceph-nvmeof.conf
    so that it matches that difference.
    
    Signed-off-by: Adam King <adking@redhat.com>
    
    Resolves: rhbz#2240588

--- src/cephadm/cephadm.py
+++ src/cephadm/cephadm.py
@@ -1017,7 +1017,7 @@ class CephNvmeof(object):
         mounts = dict()
         mounts[os.path.join(data_dir, 'config')] = '/etc/ceph/ceph.conf:z'
         mounts[os.path.join(data_dir, 'keyring')] = '/etc/ceph/keyring:z'
-        mounts[os.path.join(data_dir, 'ceph-nvmeof.conf')] = '/src/ceph-nvmeof.conf:z'
+        mounts[os.path.join(data_dir, 'ceph-nvmeof.conf')] = '/remote-source/ceph-nvmeof/app/ceph-nvmeof.conf:z'
         mounts[os.path.join(data_dir, 'configfs')] = '/sys/kernel/config'
         mounts['/dev/hugepages'] = '/dev/hugepages'
         mounts['/dev/vfio/vfio'] = '/dev/vfio/vfio'
