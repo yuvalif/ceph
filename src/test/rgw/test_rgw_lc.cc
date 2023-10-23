7d8a783deab3e5e12f2eafa0c8e21f6304036e94
Matt Benjamin <mbenjamin@redhat.com>
2023-10-23 14:57:33 -0400

    rgwlc: implement NewerNoncurrentVersions
    Per AWS doc, this value controls "how many noncurrent versions
    Amazon S3 will retain." [1]  We understand this to mean, retain
    NewerNoncurrentVersions of any object, regardless of expiration.
    
    Removed unused RGWLifecycleConfiguration::has_same_action() (cleanup)
    
    [1] https://docs.aws.amazon.com/AmazonS3/latest/API/API_NoncurrentVersionExpiration.html.
    
    Resolves: rhbz#2172162
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>
    (cherry picked from commit baef14d5739e4f35b837ea457e7dbb59a6f8635b)

--- src/test/rgw/test_rgw_lc.cc
+++ src/test/rgw/test_rgw_lc.cc
@@ -107,3 +107,32 @@ TEST(TestLCFilterInvalidAnd, XMLDoc3)
   /* check our flags */
   ASSERT_EQ(filter.get_flags(), uint32_t(LCFlagType::none));
 }
+
+static const char *xmldoc_4 =
+R"(<Rule>
+        <ID>noncur-cleanup-rule</ID>
+        <Filter>
+           <Prefix></Prefix>
+        </Filter>
+        <Status>Enabled</Status>
+       <NoncurrentVersionExpiration>
+            <NewerNoncurrentVersions>5</NewerNoncurrentVersions>
+            <NoncurrentDays>365</NoncurrentDays>
+       </NoncurrentVersionExpiration>
+    </Rule>
+)";
+
+TEST(TestLCConfigurationDecoder, XMLDoc4)
+{
+  RGWXMLDecoder::XMLParser parser;
+  ASSERT_TRUE(parser.init());
+  ASSERT_TRUE(parser.parse(xmldoc_4, strlen(xmldoc_4), 1));
+  LCRule_S3 rule;
+  auto result = RGWXMLDecoder::decode_xml("Rule", rule, &parser, true);
+  ASSERT_TRUE(result);
+  /* check results */
+  ASSERT_TRUE(rule.is_enabled());
+  const auto& noncur_expiration = rule.get_noncur_expiration();
+  ASSERT_EQ(noncur_expiration.get_days(), 365);
+  ASSERT_EQ(noncur_expiration.get_newer(), 5);
+}
