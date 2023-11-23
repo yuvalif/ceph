409209f64ab36d3d914bf24340442b6d08e4565c
Casey Bodley <cbodley@redhat.com>
2023-11-22 19:10:59 -0500

    cmake: build qat from source when WITH_SYSTEM_QATLIB=OFF
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit f34f55b48090929a8a74df934cab08c92fbed239)
    
    Resolves: rhbz#2009599

---
+++ cmake/modules/BuildQAT.cmake
@@ -0,0 +1,58 @@
+function(build_qat)
+  set(QAT_REPO https://github.com/intel/qatlib.git)
+  set(QAT_TAG "23.11.0")
+
+  include(FindMake)
+  find_make("MAKE_EXECUTABLE" "make_cmd")
+
+  set(QAT_SOURCE_DIR ${CMAKE_BINARY_DIR}/src/qatlib)
+  set(QAT_INSTALL_DIR ${QAT_SOURCE_DIR}/install)
+  set(QAT_INCLUDE_DIR ${QAT_INSTALL_DIR}/include)
+  set(QAT_LIBRARY_DIR ${QAT_INSTALL_DIR}/lib)
+  set(QAT_LIBRARY ${QAT_LIBRARY_DIR}/libqat.a)
+  set(QAT_USDM_LIBRARY ${QAT_LIBRARY_DIR}/libusdm.a)
+
+  # this include directory won't exist until the install step, but the
+  # imported targets need it early for INTERFACE_INCLUDE_DIRECTORIES
+  file(MAKE_DIRECTORY "${QAT_INCLUDE_DIR}")
+
+  set(configure_cmd env CC=${CMAKE_C_COMPILER} ./configure --prefix=${QAT_INSTALL_DIR})
+  # disable systemd or 'make install' tries to write /usr/lib/systemd/system/qat.service
+  list(APPEND configure_cmd --disable-systemd)
+  # build a static library with -fPIC that we can link into crypto/compressor plugins
+  list(APPEND configure_cmd --with-pic --enable-static --disable-shared)
+
+  set(install_cmd ${make_cmd} install)
+  # 'make install' is missing one header that ceph requires, so copy it manually
+  list(APPEND install_cmd COMMAND cmake -E copy ${QAT_SOURCE_DIR}/quickassist/utilities/libusdm_drv/include/qae_mem_utils.h ${QAT_INCLUDE_DIR}/qat)
+
+  set(source_dir_args
+    SOURCE_DIR ${QAT_SOURCE_DIR}
+    GIT_REPOSITORY ${QAT_REPO}
+    GIT_TAG ${QAT_TAG}
+    GIT_SHALLOW TRUE
+    GIT_CONFIG advice.detachedHead=false)
+
+  include(ExternalProject)
+  ExternalProject_Add(qatlib_ext
+    ${source_dir_args}
+    CONFIGURE_COMMAND ./autogen.sh COMMAND ${configure_cmd}
+    BUILD_COMMAND ${make_cmd} -j3
+    BUILD_IN_SOURCE 1
+    BUILD_BYPRODUCTS ${QAT_LIBRARY} ${QAT_USDM_LIBRARY}
+    INSTALL_COMMAND ${install_cmd}
+    UPDATE_COMMAND ""
+    LOG_CONFIGURE ON
+    LOG_BUILD ON
+    LOG_INSTALL ON
+    LOG_MERGED_STDOUTERR ON
+    LOG_OUTPUT_ON_FAILURE ON)
+  unset(make_cmd)
+
+  # export vars for find_package(QAT)
+  set(QAT_LIBRARY ${QAT_LIBRARY} PARENT_SCOPE)
+  set(QAT_USDM_LIBRARY ${QAT_USDM_LIBRARY} PARENT_SCOPE)
+  set(QAT_INCLUDE_DIR ${QAT_INCLUDE_DIR} PARENT_SCOPE)
+  # library dir for BuildQATzip.cmake
+  set(QAT_LIBRARY_DIR ${QAT_LIBRARY_DIR} PARENT_SCOPE)
+endfunction()
