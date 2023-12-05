658cbafda46b0f71dfbb2848d00297b849af5c41
kchheda3 <kchheda3@bloomberg.net>
2023-12-05 12:26:48 -0500

    rgw/multisite-notification: Add support to replicate bucket notifications in multisite config.
    Signed-off-by: kchheda3 <kchheda3@bloomberg.net>

--- src/rgw/rgw_pubsub.h
+++ src/rgw/rgw_pubsub.h
@@ -663,3 +663,23 @@ namespace rgw::notify {
   // Used in case the topic is using the default global value for dumping in a formatter
   constexpr static const std::string_view DEFAULT_CONFIG{"None"};
 }
+
+std::string topic_to_unique(const std::string& topic,
+                            const std::string& notification);
+
+std::optional<rgw_pubsub_topic_filter> find_unique_topic(
+    const rgw_pubsub_bucket_topics& bucket_topics,
+    const std::string& notif_name);
+
+// Delete the bucket notification if |notification_id| is passed, else delete
+// all the bucket notifications for the given |bucket| and update the topic
+// bucket mapping.
+int remove_notification_v2(const DoutPrefixProvider* dpp,
+                           rgw::sal::Driver* driver,
+                           rgw::sal::Bucket* bucket,
+                           const std::string& notification_id,
+                           optional_yield y);
+
+int get_bucket_notifications(const DoutPrefixProvider* dpp,
+                             rgw::sal::Bucket* bucket,
+                             rgw_pubsub_bucket_topics& bucket_topics);
