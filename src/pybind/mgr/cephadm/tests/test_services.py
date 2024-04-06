d39085a00f303b5e34e8dbb92fb6b98af7b90e7a
Aashish Sharma <aasharma@li-e74156cc-2f67-11b2-a85c-e98659a63c5c.ibm.com>
2024-04-06 08:59:13 +0530

    mgr/dashboard: add check if federate job is running else show reconnect message
    Resolves: rhbz#2278789
    
    Signed-off-by: Aashish Sharma <aasharma@redhat.com>
    (cherry picked from commit 95a5f48a317fda20c5c759b0065461f91c3f2424)
    (cherry picked from commit 9c5a31cd177991958bd8c37fe6b451714ada6af9)

--- src/pybind/mgr/cephadm/tests/test_services.py
+++ src/pybind/mgr/cephadm/tests/test_services.py
@@ -746,36 +746,39 @@ class TestMonitoring:
 
                 scrape_configs:
                   - job_name: 'ceph'
-                    honor_labels: true
                     relabel_configs:
                     - source_labels: [__address__]
                       target_label: cluster
                       replacement: fsid
+                    - source_labels: [instance]
+                      target_label: instance
+                      replacement: 'ceph_cluster'
+                    honor_labels: true
                     http_sd_configs:
                     - url: http://[::1]:8765/sd/prometheus/sd-config?service=mgr-prometheus
 
                   - job_name: 'node'
+                    relabel_configs:
+                    - source_labels: [__address__]
+                      target_label: cluster
+                      replacement: fsid
                     http_sd_configs:
                     - url: http://[::1]:8765/sd/prometheus/sd-config?service=node-exporter
-                    relabel_configs:
-                    - source_labels: [__address__]
-                      target_label: cluster
-                      replacement: fsid
 
                   - job_name: 'haproxy'
+                    relabel_configs:
+                    - source_labels: [__address__]
+                      target_label: cluster
+                      replacement: fsid
                     http_sd_configs:
                     - url: http://[::1]:8765/sd/prometheus/sd-config?service=haproxy
-                    relabel_configs:
-                    - source_labels: [__address__]
-                      target_label: cluster
-                      replacement: fsid
 
                   - job_name: 'ceph-exporter'
-                    honor_labels: true
                     relabel_configs:
                     - source_labels: [__address__]
                       target_label: cluster
                       replacement: fsid
+                    honor_labels: true
                     http_sd_configs:
                     - url: http://[::1]:8765/sd/prometheus/sd-config?service=ceph-exporter
 
@@ -783,18 +786,6 @@ class TestMonitoring:
                     http_sd_configs:
                     - url: http://[::1]:8765/sd/prometheus/sd-config?service=nvmeof
 
-                  - job_name: 'federate'
-                    scrape_interval: 15s
-                    honor_labels: true
-                    metrics_path: '/federate'
-                    params:
-                      'match[]':
-                        - '{job="ceph"}'
-                        - '{job="node"}'
-                        - '{job="haproxy"}'
-                        - '{job="ceph-exporter"}'
-                    static_configs:
-                    - targets: []
                 """).lstrip()
 
                 _run_cephadm.assert_called_with(
@@ -886,6 +877,8 @@ class TestMonitoring:
                 global:
                   scrape_interval: 10s
                   evaluation_interval: 10s
+                  external_labels:
+                    cluster: fsid
 
                 rule_files:
                   - /etc/prometheus/alerting/*
@@ -908,6 +901,13 @@ class TestMonitoring:
 
                 scrape_configs:
                   - job_name: 'ceph'
+                    relabel_configs:
+                    - source_labels: [__address__]
+                      target_label: cluster
+                      replacement: fsid
+                    - source_labels: [instance]
+                      target_label: instance
+                      replacement: 'ceph_cluster'
                     scheme: https
                     tls_config:
                       ca_file: mgr_prometheus_cert.pem
@@ -921,6 +921,10 @@ class TestMonitoring:
                         ca_file: root_cert.pem
 
                   - job_name: 'node'
+                    relabel_configs:
+                    - source_labels: [__address__]
+                      target_label: cluster
+                      replacement: fsid
                     scheme: https
                     tls_config:
                       ca_file: root_cert.pem
@@ -933,6 +937,10 @@ class TestMonitoring:
                         ca_file: root_cert.pem
 
                   - job_name: 'haproxy'
+                    relabel_configs:
+                    - source_labels: [__address__]
+                      target_label: cluster
+                      replacement: fsid
                     scheme: https
                     tls_config:
                       ca_file: root_cert.pem
@@ -945,6 +953,10 @@ class TestMonitoring:
                         ca_file: root_cert.pem
 
                   - job_name: 'ceph-exporter'
+                    relabel_configs:
+                    - source_labels: [__address__]
+                      target_label: cluster
+                      replacement: fsid
                     honor_labels: true
                     scheme: https
                     tls_config:
